016693e17dd72ac7d7ac6e2159d52b27d75e4170
Ashimory <47056302+Ashimory@users.noreply.github.com>
2020-06-16 00:53:52 +0700

    Add files via upload

---
+++ Homework-Session1/Homework-Session1/circleArea/area.js
@@ -0,0 +1,4 @@
+const radius = prompt("Enter circle radius")
+let result = eval(radius*radius*Math.PI)
+alert(`The area of the circle is ${result}`)
+
---
+++ Homework-Session1/Homework-Session1/temparatureConverter/tempConverter.js
@@ -0,0 +1,3 @@
+const Celsius = prompt("Enter temparature in Celsius")
+let result = eval(Celsius*1.8+32)
+alert(`${Celsius}°C = ${result}°F`)
