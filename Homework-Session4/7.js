68697e3895e515b200c16e118672bc4194043312
Ashimory <duyminhlugia@gmail.com>
2020-06-30 12:21:14 +0700

    Update 7.js

--- Homework-Session4/7.js
+++ Homework-Session4/7.js
@@ -1,8 +1,8 @@
-let size = prompt("How many numbers do you want in each chunk?");
+let size = Number(prompt("How many numbers do you want in each chunk?"));
 let array = prompt("Enter your series of numbers, separated by a comma").split(",");
 let result = [];    
 for (let i = 0; i < array.length; i += size) {
-        let chunk = array.slice(i, i + size);
+        chunk = array.slice(i, i + size);
         result.push(chunk);
     };
 console.log(result);
